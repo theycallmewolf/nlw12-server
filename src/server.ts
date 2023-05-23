9333812d5f6fe921a5af068c36d3d1ae1a92e563
They Call Me Wolf <bruno.m.lobato@gmail.com>
2023-05-23 18:58:24 +0100

    `fastify` setup

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,21 @@
-const greeting: string = "hello wolf";
+import fastify from "fastify";
 
-console.log(greeting);
+const app = fastify();
+
+app.get("/hello", () => {
+  return "Hello Wolf!";
+});
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.info(`
+  
+    🐺 HTTP server running on http://localhost:3333
+  
+  `);
+  });
+
+  
