114067315188531f7b821b1fade15b11db3c7a46
They Call Me Wolf <bruno.m.lobato@gmail.com>
2023-05-25 11:36:57 +0100

    `Prisma` setup

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 const app = fastify()
+const prisma = new PrismaClient()
 
-app.get('/hello', () => {
-  return 'Hello Wolf!'
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+  return users
 })
 
 app
@@ -14,6 +17,6 @@ app
     console.info(`
   
     🐺 HTTP server running on http://localhost:3333
-  
+   
   `)
   })
